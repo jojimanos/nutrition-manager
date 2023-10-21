4558eba26906829e8818c6263d13166ed1aefc9e
jojimanos <manosgeorge7@gmail.com>
2023-10-21 22:39:37 +0300

    landing screen and styles

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -15,7 +15,7 @@ const LandingScreen: React.FC<LandingScreenProps> = () => {
     const [foodType, setFoodType] = useState("Survey (FNDDS)")
 
     async function getNutritionalValue(food: string) {
-        const response = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=&query=${food}&dataType=${foodType}`,
+        const response = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${process.env.API_KEY}&query=${food}&dataType=${foodType}`,
         );
         const item = await response.json();
         setProtein(JSON.stringify(item.foods[0].foodNutrients[0].value))
